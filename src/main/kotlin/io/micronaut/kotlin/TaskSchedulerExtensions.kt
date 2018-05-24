8488d58016d5bf7832d6613a999047e430b8f778
Alejandro Gomez <ideaplugins@gmail.com>
2018-05-24 11:48:13 -0300

    Add TaskScheduler extensions

---
+++ src/main/kotlin/io/micronaut/kotlin/TaskSchedulerExtensions.kt
@@ -0,0 +1,21 @@
+package io.micronaut.kotlin
+
+import io.micronaut.scheduling.TaskScheduler
+import java.time.Duration
+import java.util.concurrent.Callable
+
+/**
+ * Extension for [TaskScheduler.schedule] providing a `scheduleCallable("cron") { doWork() }` variant.
+ *
+ * @author Alejandro Gomez
+ * @since 1.0
+ */
+inline fun <V> TaskScheduler.scheduleCallable(cron: String, crossinline command: () -> V) = schedule(cron, Callable<V> { command.invoke() })
+
+/**
+ * Extension for [TaskScheduler.schedule] providing a `scheduleCallable(delay) { doWork() }` variant.
+ *
+ * @author Alejandro Gomez
+ * @since 1.0
+ */
+inline fun <V> TaskScheduler.scheduleCallable(delay: Duration, crossinline command: () -> V) = schedule(delay, Callable<V> { command.invoke() })
